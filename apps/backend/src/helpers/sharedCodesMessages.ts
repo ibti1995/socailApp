b83b4c628012681cb5b38c30f9bf42fbb58b9360
ibti1995 <ibtissembouchnak@gmail.com>
2024-09-29 01:45:22 +0100

    Add helpers

---
+++ apps/backend/src/helpers/sharedCodesMessages.ts
@@ -0,0 +1,25 @@
+// sharedMessages.ts
+
+export const msgCode = {
+  dataRetrievalError: 'ERR001',
+  emailAlredyExistsError: 'ERR002',
+  dataRetrievalSuccess: 'SUCC001',
+  dataAlredyDeletedError: 'ERR003',
+  dataDeletedSuccess: 'SUCC003',
+  dataUpdatedSuccess: 'SUCC004',
+  addDataError: 'ERR004',
+
+};
+
+export const messages = {
+  dataRetrievalError: 'Cannot retrieve data',
+  dataRetrievalSuccess: 'Data retrieved successfully',
+  dataPostLikeSuccess: 'Post liked successfully',
+  emailAlredyExistsError: 'Email alredy exists',
+  dataAlredyExistsError: 'Data alredy exists',
+  dataAlredyDeletedError: 'Data notfound or alredy deleted',
+  dataDeletedSuccess: 'Data successfully deleted',
+  dataUpdatedSuccess: 'Data successfully updated',
+  addDataError: 'Cannot add data',
+
+};
